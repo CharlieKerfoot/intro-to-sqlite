43be24d201501d59f1eeeaca67ef693425c5be1f
Charlie Kerfoot <charliekerfoot@Charlies-Air-2.lan>
2024-12-12 21:07:36 -0500

    added sql crud endpoints

--- db.ts
+++ db.ts
@@ -1,17 +1,26 @@
 //-----------------------------
 //#region Database Connection
 //-----------------------------
-import path from "path";
-import {Request, Response} from 'express';
-import sqlite3 from "sqlite3";
+import path from 'path';
+import { json, Request, Response } from 'express';
+import sqlite3 from 'sqlite3';
 const sqlite = sqlite3.verbose();
-const dbFile = path.join(__dirname, "foo.db");
+const filename = 'foo';
+const dbFile = path.join(__dirname, filename + '.db');
 // below is the line for vanilla ES6 js to work; not necessary with typescript
 // const dbFile = path.join(path.dirname(fileURLToPath(import.meta.url)), "foo.db");
 const db = new sqlite.Database(dbFile, (error) => {
-  if (error) return console.error(error.message);
-  console.log(`Connected to database ${dbFile}`);
+	if (error) return console.error(error.message);
+	console.log(`Connected to database ${dbFile}`);
 });
+
+const handleError = (error: Error | null, response: Response) => {
+	if (error) {
+		console.error(error.message);
+		response.status(400).json({ error: error.message });
+		return;
+	}
+};
 
 //#endregion Database Connection
 
@@ -21,29 +30,112 @@ const db = new sqlite.Database(dbFile, (error) => {
 /**
  * Gets a single user by id
  */
-export const getUserById = (request: Request, response: Response) => {
-  // Parse the id to generate a SQLite query
-  const id = parseInt(request.params.id);
-  const query = `SELECT * FROM user WHERE id = ?`;
+export const getUserById = (request: Request, response: Response): void => {
+	// Parse the id to generate a SQLite query
+	const id: number = parseInt(request.params.id);
+	const query: string = `SELECT * FROM user WHERE id = ?`;
 
-  // db.get will replace all ? in query sequentially with
-  // items from the array passed as the second parameter
-  // and then run the callback function passed as the third param
-  // What does the callback function do?
-  db.get(query, [id], (error, result) => {
-    if (error) {
-      console.error(error.message);
-      response.status(400).json({ error: error.message });
-      return;
-    }
-    // If nothing is returned, then result will be undefined
-    if (result) {
-      response.json(result);
-    } else {
-      response.sendStatus(404);
-    }
-  });
+	// db.get will replace all ? in query sequentially with
+	// items from the array passed as the second parameter
+	// and then run the callback function passed as the third param
+	// What does the callback function do?
+	db.get(query, [id], (error: Error, result: object): void => {
+		handleError(error, response);
+		// If nothing is returned, then result will be undefined
+		if (result) {
+			response.json(result);
+		} else {
+			response.sendStatus(404);
+		}
+	});
 };
 
-// ----- FILL IN BELOW -----
-// Write and export the rest of the functions needed by index.js!
+export const getAllUsers = (request: Request, response: Response): void => {
+	const query: string = `SELECT * FROM user`;
+
+	db.all(query, (error: Error, result: object): void => {
+		handleError(error, response);
+
+		if (result) {
+			response.json(result);
+		} else {
+			response.sendStatus(404);
+		}
+	});
+};
+
+export const createUser = (request: Request, response: Response): void => {
+	const query: string = 'INSERT INTO user(id, name)  Values(?, ?)';
+	if (!request.body.name || !request.params.id)
+		response.status(400).json({ error: 'Incorrect Parameters' });
+	const id: number = parseInt(request.params.id);
+	const name: string = request.body.name;
+
+	db.run(query, [id, name], (error: Error): void => {
+		handleError(error, response);
+
+		response.status(201).json({ message: 'User Successfully Created' });
+	});
+};
+
+const checkUser = (id: number): object | null => {
+	const query: string = `SELECT * FROM user WHERE id = ?`;
+	return new Promise((resolve, reject) => {
+		db.get(query, [id], (error, result): void => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(result || null);
+			}
+		});
+	});
+};
+
+export const updateUser = async (request: Request, response: Response): Promise<void> => {
+	const query: string = 'UPDATE user SET name = ? WHERE id = ?';
+	if (!request.body.name || !request.params.id)
+		response.status(400).json({ error: 'Incorrect Parameters' });
+	const id: number = parseInt(request.params.id);
+	const name: string = request.body.name;
+
+	try {
+		const user = await checkUser(id);
+		if (!user) {
+			response.status(404).json({ error: 'User Does Not Exist' });
+			return;
+		}
+
+		db.run(query, [name, id], (error): void => {
+			handleError(error, response);
+
+			response.status(200).json({ message: 'User Successfully Updated' });
+		});
+	} catch (error) {
+		console.error(error);
+		response.status(500).json({ error: 'Internal Server Error' });
+	}
+};
+
+export const deleteUser = async (request: Request, response: Response): Promise<void> => {
+	const query: string = 'DELETE FROM user WHERE id = ?';
+	if (!request.params.id)
+		response.status(400).json({ error: 'Incorrect Parameters' });
+	const id: number = parseInt(request.params.id);
+
+	try {
+		const user = await checkUser(id);
+		if (!user) {
+			response.status(404).json({ error: 'User Does Not Exist' });
+			return;
+		}
+
+		db.run(query, [id], (error: Error): void => {
+			handleError(error, response);
+
+			response.status(200).json({ message: 'User Successfully Deleted' });
+		});
+	} catch (error) {
+		console.error(error);
+		response.status(500).json({ error: 'Internal Server Error' });
+	}
+};
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 //-----------------------------
 // #region Setup
 //-----------------------------
-import express from "express";
-import * as db from "./db"; 
+import express from 'express';
+import * as db from './db';
 const app = express();
 const PORT = 4000;
 //#endregion Setup
@@ -18,43 +18,49 @@ app.use(express.json());
 // For more information about CORS (Cross-Origin Resource Sharing):
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use((req, res, next) => {
-  // Allow access from multiple origins
-  const allowedOrigins = [
-    "http://localhost:8080",
-  ];
-  const origin = req.headers.origin;
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  // Allow specific requests
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	// Allow access from multiple origins
+	const allowedOrigins = ['http://localhost:8080'];
+	const origin = req.headers.origin;
+	if (origin && allowedOrigins.includes(origin)) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	// Allow specific requests
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Pass to next layer of middleware
-  next();
+	// Pass to next layer of middleware
+	next();
 });
 //#endregion App Config
 
 //-----------------------------
 //#region Database Routes
 //-----------------------------
-app.get("/", (req, res) => {
-  res.json({ info: "Demo app for sqlite3" });
+app.get('/', (req, res) => {
+	res.json({ info: 'Demo app for sqlite3' });
 });
 
 // This endpoint allows a client to get a single user by id
-app.get("/user/:id", db.getUserById);
+app.get('/user/:id', db.getUserById);
 
 // ------ FILL IN BELOW -------
 // Write endpoints that allow a client to:
 
 // Get all users
 
+app.get('/all-users', db.getAllUsers);
+
 // Create a new user
+
+app.post('/user/:id', db.createUser);
 
 // Update a user's name, given an id
 
+app.put('/user/:id', db.updateUser);
+
 // Delete a user by id
+
+app.delete('/user/:id', db.deleteUser);
 
 //#endregion Database Routes
 
@@ -62,6 +68,6 @@ app.get("/user/:id", db.getUserById);
 //#region Server
 //-----------------------------
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+	console.log(`Listening on port ${PORT}`);
 });
 //#endregion Server
